3e43f7f639124d4cb8b4e517561ef888b154675b
George Hamilton <gmh04@netscape.net>
2018-04-04 16:42:25 +0100

    move all ui references out of map module and improve look of overlay thumbnails

--- js/map.js
+++ js/map.js
@@ -3,11 +3,10 @@
 /* global Cesium */
 
 define(function(require) {
-    var config = require('config');
-
-    var EVT_LINE_ADDED = 'EVT_LINE_ADDED';
-    var EVT_POINT_ADDED = 'EVT_POINT_ADDED';
     var EVT_DELETE_LINE = 'EVT_DELETE_LINE';
+    var EVT_LINE_ADDED  = 'EVT_LINE_ADDED';
+    var EVT_MAP_CLICK   = 'EVT_MAP_CLICK';
+    var EVT_POINT_ADDED = 'EVT_POINT_ADDED';
 
     var drawLine = false;
 
@@ -20,13 +19,15 @@ define(function(require) {
 
     Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
     Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
+    Cesium.BingMapsApi.defaultKey = null;
 
     var viewer = new Cesium.Viewer('cesiumContainer', {
         requestRenderMode: true,
         imageryProvider : new Cesium.createOpenStreetMapImageryProvider({
             url : 'https://a.tile.openstreetmap.org/'
         }),
-        baseLayerPicker: false
+        baseLayerPicker: false,
+        geocoder: false // bing api used in geocoder
     });
     viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
     viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
@@ -126,47 +127,37 @@ define(function(require) {
         }
     });
 
-    $(document).on('click', '.thumb', function() {
-        var layer = scene.imageryLayers.addImageryProvider(new Cesium.SingleTileImageryProvider({
-            url: $(this).attr('data-field-large-thumb'),
-            rectangle: Cesium.Rectangle.fromDegrees(
-                $(this).attr('data-field-w'),
-                $(this).attr('data-field-s'),
-                $(this).attr('data-field-e'),
-                $(this).attr('data-field-n')
-            )
-        }));
-        layer.alpha = 0.5;
-    });
-
-    var displayAvailableTiles = function(lon, lat) {
-        $.getJSON(config.backend_url + "/landsat", {
-            lon: lon, lat: lat
-        }).done(function(data) {
-            $('#available-tiles').empty();
-
-            // TODO: just showing first 10
-            $.each(data.msg.slice(0, 10), function(i, entry) {
-                var index = entry.download_url;
-                var imagePath = index.substr(0, index.lastIndexOf('/'));
-                var productId = index.split('/')[8];
-                var sThumb = imagePath + '/' + productId + '_thumb_small.jpg';
-                var lThumb = imagePath + '/' + productId + '_thumb_large.jpg';
-                $('#available-tiles').append('<div class="thumb" data-field-large-thumb="' + lThumb +
-                                             '" data-field-e="' + entry.max_lon +
-                                             '" data-field-w="' + entry.min_lon +
-                                             '" data-field-n="' + entry.max_lat +
-                                             '" data-field-s="' + entry.min_lat +
-                                             '"><img src="' + sThumb + '"></img></div>');
-            });
-        });
-    };
-
     var clearLine = function(text) {
         entities.removeAll();
         viewer.scene.requestRender();
     };
 
+    var displayAvailableTiles = function(lon, lat) {
+        $.event.trigger(
+            {
+                type: EVT_MAP_CLICK
+            },
+            {
+                lon: lon,
+                lat: lat
+            }
+        );
+    };
+
+    var displayOverlay = function(url, extents) {
+        var layer = scene.imageryLayers.addImageryProvider(
+            new Cesium.SingleTileImageryProvider({
+                url: url,
+                rectangle: Cesium.Rectangle.fromDegrees(
+                    extents.west,
+                    extents.south,
+                    extents.east,
+                    extents.north
+                )
+            })
+        );
+        layer.alpha = 0.5;
+    };
 
     var highlightLine = function(){
         $.each(entities.values, function(i, entity){
@@ -211,10 +202,13 @@ define(function(require) {
     };
 
     return {
+        EVT_DELETE_LINE: EVT_DELETE_LINE,
+        EVT_MAP_CLICK: EVT_MAP_CLICK,
         EVT_LINE_ADDED: EVT_LINE_ADDED,
         EVT_POINT_ADDED: EVT_POINT_ADDED,
-        EVT_DELETE_LINE: EVT_DELETE_LINE,
+
         clearLine: clearLine,
+        displayOverlay: displayOverlay,
         setDrawLine: function(draw) {
             drawLine = draw;
         }
--- js/panel.js
+++ js/panel.js
@@ -1,12 +1,17 @@
 'use strict';
 
 define(function(require) {
+    var config = require('config');
     var map = require('map');
 
     var displayPointDetails = function(text) {
         $('#draw-line-details-text').text(text);
         $('#draw-line-details').show();
     };
+
+    $(document).on(map.EVT_DELETE_LINE, function() {
+        $('#draw-line-details').hide();
+    });
 
     $(document).on(map.EVT_LINE_ADDED, function(evt, line) {
         displayPointDetails(
@@ -15,13 +20,48 @@ define(function(require) {
         );
     });
 
+    $(document).on(map.EVT_MAP_CLICK, function(evt, point) {
+        $('#available-tiles').empty();
+        $.getJSON(config.backend_url + "/landsat", {
+            lon: point.lon, lat: point.lat
+        }).done(function(data) {
+            // TODO: just showing first 10
+            $.each(data.msg.slice(0, 10), function(i, entry) {
+                var index = entry.download_url;
+                var imagePath = index.substr(0, index.lastIndexOf('/'));
+                var productId = index.split('/')[8];
+                var sThumb = imagePath + '/' + productId + '_thumb_small.jpg';
+                var lThumb = imagePath + '/' + productId + '_thumb_large.jpg';
+                $('#available-tiles').append(
+                    '<div class="thumb loader" data-field-large-thumb="' + lThumb +
+                        '" data-field-e="' + entry.max_lon +
+                        '" data-field-w="' + entry.min_lon +
+                        '" data-field-n="' + entry.max_lat +
+                        '" data-field-s="' + entry.min_lat +
+                        '"><img src="' + sThumb + '"></img><div id="thumb-meta"><div id=thumb-meta-acquisitionDate>' +
+                        entry.acquisitionDate + '</div></div></div>');
+            });
+        });
+    });
+
+
+    $(document).on('click', '.thumb', function() {
+        map.displayOverlay(
+            $(this).attr('data-field-large-thumb'),
+            {
+                west: $(this).attr('data-field-w'),
+                south: $(this).attr('data-field-s'),
+                east: $(this).attr('data-field-e'),
+                north: $(this).attr('data-field-n')
+            }
+        );
+    });
+
+
     $(document).on(map.EVT_POINT_ADDED, function(evt, point) {
         displayPointDetails(
             'POINT (' + point.lon.toFixed(2) + ' ' + point.lat.toFixed(2) + ')'
         );
     });
 
-    $(document).on(map.EVT_DELETE_LINE, function() {
-        $('#draw-line-details').hide();
-    });
 });
