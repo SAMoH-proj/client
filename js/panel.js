da98ca8f167df3d999501832c3d7df9829704cca
George Hamilton <gmh04@netscape.net>
2018-04-11 12:34:27 +0100

    add extra metadata collapsible pane to available tiles panel #2

--- js/panel.js
+++ js/panel.js
@@ -45,29 +45,99 @@ define(function(require) {
                 var productId = index.split('/')[8];
                 var sThumb = imagePath + '/' + productId + '_thumb_small.jpg';
                 var lThumb = imagePath + '/' + productId + '_thumb_large.jpg';
-                $('#available-tiles').append(
-                    '<div class="thumb loader" data-field-large-thumb="' + lThumb +
-                        '" data-field-e="' + entry.max_lon +
-                        '" data-field-w="' + entry.min_lon +
-                        '" data-field-n="' + entry.max_lat +
-                        '" data-field-s="' + entry.min_lat +
-                        '"><img src="' + sThumb +
-                        '"></img><div id="thumb-meta"><div id=thumb-meta-acquisitionDate>' +
-                        entry.acquisitionDate + '</div></div></div>');
+
+                $('#available-tiles').append(`
+                    <div class="thumb loader"
+                         data-field-large-thumb="${lThumb}"
+                         data-field-e="${entry.max_lon}"
+                         data-field-w="${entry.min_lon}"
+                         data-field-n="${entry.max_lat}"
+                         data-field-s="${entry.min_lat}">
+                      <img src="${sThumb}"></img>
+
+                    <div class="thumb-meta">
+                      <div title="Show/Hide image metadata"
+                         class="thumb-meta-more">more</div>
+                      <table class="table">
+                        <tbody>
+                          <tr>
+                            <th>Acquisition Date</th>
+                            <th>${entry.acquisitionDate}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Cloud Cover</th>
+                            <th>${entry.cloudCover}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Entity ID</th>
+                            <th>${entry.entityId}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Epoch</th>
+                            <th>${entry.epoch}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Minimum Latitude</th>
+                            <th>${entry.min_lat}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Minimum Longitude</th>
+                            <th>${entry.min_lon}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Maximum Latitude</th>
+                            <th>${entry.max_lat}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Maximum Longitude</th>
+                            <th>${entry.max_lon}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Processing Level</th>
+                            <th>${entry.processingLevel}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Product ID</th>
+                            <th>${entry.productId}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Path</th>
+                            <th>${entry.path}</th>
+                          </tr>
+                          <tr class="collapsible">
+                            <th>Row</th>
+                            <th>${entry.row}</th>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>`
+                );
             });
         });
     });
 
-    $(document).on('click', '.thumb', function() {
+    $(document).on('click', '.thumb img', function() {
+        var div = $(this).parent();
         map.displayOverlay(
-            $(this).attr('data-field-large-thumb'),
+            div.attr('data-field-large-thumb'),
             {
-                west: $(this).attr('data-field-w'),
-                south: $(this).attr('data-field-s'),
-                east: $(this).attr('data-field-e'),
-                north: $(this).attr('data-field-n')
+                west: div.attr('data-field-w'),
+                south: div.attr('data-field-s'),
+                east: div.attr('data-field-e'),
+                north: div.attr('data-field-n')
             }
         );
+    });
+
+    $(document).on('click', '.thumb-meta-more', function(evt) {
+        if ($(this).text() === 'more') {
+            $(this).siblings().find('.collapsible').show();
+            $(this).text('less');
+        }
+        else {
+            $(this).siblings().find('.collapsible').hide();
+            $(this).text('more');
+        }
     });
 
     $(document).on(map.EVT_POINT_ADDED, function(evt, point) {
